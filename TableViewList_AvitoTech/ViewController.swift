0f37694eedd5c549cf37fdc651b99179529bb61a
ekaterina-ivanova-ios <hard123hatiko@gmail.com>
2022-10-21 13:30:32 +0300

    Initial Commit

---
+++ TableViewList_AvitoTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewList_AvitoTech
+//
+//  Created by Екатерина Иванова on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
