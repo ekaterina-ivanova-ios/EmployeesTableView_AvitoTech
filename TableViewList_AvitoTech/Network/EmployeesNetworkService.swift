592e56c1fbfdd14908b0c9b31fbc7ae498b2b782
ekaterina-ivanova-ios <hard123hatiko@gmail.com>
2022-10-27 00:18:34 +0300

    Modification for data loading. Added activityIndicator.

--- TableViewList_AvitoTech/Network/EmployeesNetworkService.swift
+++ TableViewList_AvitoTech/Network/EmployeesNetworkService.swift
@@ -17,24 +17,7 @@ final class EmployeesNetworkService {
                     completionHandler(.failure(.decodeError))
                     return
                 }
-                
-                //конвертируем данные из структуры CommonData в структуру DataEmployeesModel
-                for employee in decodedResponse.company.employees {
-                    let model = DataEmployeesModel(nameCompany: decodedResponse.company.name,
-                                                   nameEmployees: employee.name,
-                                                   phoneEmployees: employee.phoneNumber,
-                                                   skillsEmployees: employee.skills)
-                    //добавляем в массив данные
-                    self.employeesList?.append(model)
-                }
-                
-                //сортируем массив с работниками по именам в алфавитном порядке
-                self.employeesSorted = (self.employeesList?.sorted {
-                    guard let firstName = $0.nameEmployees else { return false }
-                    guard let secondName = $1.nameEmployees else { return true }
-                    return firstName < secondName
-                })
-                
+    
                 completionHandler(.success(decodedResponse))
                 
             case .failure(let error):
--- TableViewList_AvitoTech/Representor/EmployeerViewController.swift
+++ TableViewList_AvitoTech/Representor/EmployeerViewController.swift
@@ -9,23 +9,18 @@ final class EmployeerViewController: UIViewController {
     var employeesSorted = [DataEmployeesModel]()
     //создаем тэйбл вью
     let employeesTableView = UITableView()
-    //создаем экземпляр класс URLSessionConfiguration
-    let sessionConfiguration = URLSessionConfiguration.default
     //создаем экземпляр сеанса
     let session = URLSession.shared
     //создаем экземпляр декодера
     let decoder = JSONDecoder()
-    //url
-    let url = URL(string: "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c")
-    //создаем переменную для хранения кэша
-    private let cache = NSCache<NSString, CommonData>()
-    //создаем индикатор загрузки
-    private var activityIndicator: UIActivityIndicatorView!
+    //создаем индикатор
+    let myActivityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(employeesTableView)
+        view.addSubview(myActivityIndicator)
         
         employeesTableView.delegate = self
         employeesTableView.dataSource = self
@@ -33,20 +28,71 @@ final class EmployeerViewController: UIViewController {
         //настраиваем тэйбл вью
         settingTableView()
         
+        //настраиваем индикатор
+        showActivityIndicator()
+        
         employeesTableView.register(EmployeesTableViewCell.self, forCellReuseIdentifier: "employeeCell")
         navigationItem.title = "EmployeesList"
         
-        //загружаем данные
-        //fetchData()
-        
         let employeesNetworkService = EmployeesNetworkService(networkManager: NetworkManager())
-        employeesNetworkService.fetchData { res in
+        employeesNetworkService.fetchData { [weak self] res in
+            self?.hideActivityIndicator()
+            guard let self = self else { return }
             print(res)
+            switch res {
+            case .success(let value):
+                // здесь достаем данные из value(это ассоциированное значение, в нем передаются данные полученные с сервера)
+                for employee in value.company.employees {
+                    // преобразуем в подходящую модель
+                    let employee = DataEmployeesModel(nameCompany: value.company.name,
+                                                      nameEmployees: employee.name,
+                                                      phoneEmployees: employee.phoneNumber,
+                                                      skillsEmployees: employee.skills)
+                    
+                    
+                    // добавляем в массив
+                    self.employees.append(employee)
+                }
+                
+                //сортируем массив с работниками по именам в алфавитном порядке
+                self.employeesSorted = (self.employees.sorted {
+                    guard let firstName = $0.nameEmployees else { return false }
+                    guard let secondName = $1.nameEmployees else { return true }
+                    return firstName < secondName
+                })
+                
+                // обновляем таблицу после того как заполнили наш массив
+                // здесь нужно немного почитать про многопоточность, в кратце этот блок(кложур у fetchData) выполняется не на главном потоке, а любые UI изменения(например обновление таблицы) нужно осуществлять на главном потоке, поэтому мы переведем обновление таблицы на главный поток
+                DispatchQueue.main.async {
+                    self.employeesTableView.reloadData()
+                }
+            case .failure(let error):
+                // здесь получаем ошибку и можем показать алерт с этой ошибкой
+                print(error)
+                switch error {
+                case .decodeError:
+                    print("decodeError")
+                case .invalidUrl:
+                    print("invalidUrl")
+                case .networkTaskError:
+                    print("networkTaskError")
+                }
+            }
         }
-
-        
-        
-        
+    }
+    
+    //MARK: setting constraints for table view
+    private func showActivityIndicator() {
+        myActivityIndicator.center = view.center
+        myActivityIndicator.hidesWhenStopped = false
+        myActivityIndicator.startAnimating()
+    }
+    
+    private func hideActivityIndicator() {
+        DispatchQueue.main.async {
+            self.myActivityIndicator.isHidden = true
+            self.myActivityIndicator.stopAnimating()
+        }
     }
     
     //MARK: setting constraints for table view
@@ -77,82 +123,5 @@ extension EmployeerViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
-    
 }
 
-//MARK: GET data
-extension EmployeerViewController {
-    
-    func fetchData() {
-        
-        guard let url = url else {return}
-        
-        session.dataTask(with: url) { [weak self] (data, response, error) in
-            guard let strongSelf = self else {return}
-            //проверяем, есть ли ошибка
-            if error == nil, let dataNotNil = data {
-                //пытаемся раcпарсить данные из json
-                let responseParse = try? strongSelf.decoder.decode(CommonData.self, from: dataNotNil)
-                //unwrap optional type
-                guard let result = responseParse else {return}
-                
-                
-                //конвертируем данные из структуры CommonData в структуру DataEmployeesModel
-                for employee in result.company.employees {
-                    let model = DataEmployeesModel(nameCompany: result.company.name,
-                                                   nameEmployees: employee.name,
-                                                   phoneEmployees: employee.phoneNumber,
-                                                   skillsEmployees: employee.skills)
-                    //добавляем в массив данные
-                    self?.employees.append(model)
-                }
-                
-                //сортируем массив с работниками по именам в алфавитном порядке
-                self?.employeesSorted = (self?.employees.sorted {
-                    guard let firstName = $0.nameEmployees else { return false }
-                    guard let secondName = $1.nameEmployees else { return true }
-                    return firstName < secondName
-                } ?? [])
-                //обновляем вью
-                DispatchQueue.main.async {
-                    self?.employeesTableView.reloadData()
-                }
-                
-            } else {
-                print("Error: Data didn't load")
-            }
-        }.resume()
-        
-    }
-    
-}
-
-//extension EmployeerViewController {
-//
-//    func fetchData(completion: @escaping (CommonData) -> Void) {
-//
-//        guard let url = url else {return}
-//
-//        let request = URLRequest(url: url, cachePolicy: URLRequest.CachePolicy.returnCacheDataElseLoad, timeoutInterval: 60*60)
-//        let dataTask = session.dataTask(with: request) { [weak self] data, response, error in
-//
-//            guard error == nil,
-//                  data != nil,
-//                  let response = response as? HTTPURLResponse,
-//                  response.statusCode == 200,
-//                  let self =  self else {
-//                return
-//            }
-//
-//            guard let data = CommonData(from: <#T##Decoder#>) else {return}
-//
-//            DispatchQueue.main.async {
-//                completion(data)
-//            }
-//
-//
-//        }
-//
-//    }
-//
-//}
