4640778df6e1e4f0f2e50c6b2ece293289c01b99
ekaterina-ivanova-ios <hard123hatiko@gmail.com>
2022-10-27 23:55:31 +0300

    Add cache

--- TableViewList_AvitoTech/Representor/EmployeerViewController.swift
+++ TableViewList_AvitoTech/Representor/EmployeerViewController.swift
@@ -3,16 +3,25 @@ import UIKit
 
 final class EmployeerViewController: UIViewController {
     
+    //создаем массив с работниками
     private var employees = [DataEmployeesModel]()
+    //создаем массив с работниками, отсортированный по имени в алфавитном порядке
     private var employeesSorted = [DataEmployeesModel]()
+    //создаем тэйбл вью
     private let employeesTableView = UITableView()
-    private let session = URLSession.shared
-    private let decoder = JSONDecoder()
+    //создаем индикатор загрузки
     private let myActivityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+    //создаем экземпляр для хранения кэша
+    private let cache = Cache<String, [DataEmployeesModel]>()
+    //создаем url
+    private let urlString = "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c"
+    //создаем экземпляр EmployeesNetworkService
+    private let employeesNetworkService = EmployeesNetworkService(networkManager: NetworkManager())
+    
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .darkContent
     }
-    private let cache = Cache<String, [DataEmployeesModel]>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,71 +32,26 @@ final class EmployeerViewController: UIViewController {
         employeesTableView.delegate = self
         employeesTableView.dataSource = self
         
+        //настраиваем тэйбл вью
         settingTableView()
-        
+        //запускаем индикатор
         showActivityIndicator()
         
         employeesTableView.register(EmployeesTableViewCell.self, forCellReuseIdentifier: "employeeCell")
         navigationItem.title = "EmployeesList"
         
-        let employeesNetworkService = EmployeesNetworkService(networkManager: NetworkManager())
-        
-        if cache.value(forKey: "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c") == nil {
-            
-            employeesNetworkService.fetchData { [weak self] res in
-                self?.hideActivityIndicator()
-                guard let self = self else { return }
-                print(res)
-                switch res {
-                case .success(let value):
-                    for employee in value.company.employees {
-                        let employee = DataEmployeesModel(nameCompany: value.company.name,
-                                                          nameEmployees: employee.name,
-                                                          phoneEmployees: employee.phoneNumber,
-                                                          skillsEmployees: employee.skills)
-                        
-                        self.employees.append(employee)
-                    }
-                    
-                    self.employeesSorted = (self.employees.sorted {
-                        guard let firstName = $0.nameEmployees else { return false }
-                        guard let secondName = $1.nameEmployees else { return true }
-                        return firstName < secondName
-                    })
-                    
-                    self.cache.insert(self.employeesSorted, forKey: "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c")
-                    
-                    DispatchQueue.main.async {
-                        self.employeesTableView.reloadData()
-                    }
-                    
-                case .failure(let error):
-                    switch error {
-                    case .decodeError:
-                        DispatchQueue.main.async {
-                            self.showAlert(error: .decodeError)
-                            print("decodeError")
-                        }
-                    case .invalidUrl:
-                        DispatchQueue.main.async {
-                            self.showAlert(error: .invalidUrl)
-                            print("invalidUrl")
-                        }
-                    case .networkTaskError:
-                        DispatchQueue.main.async {
-                            self.showAlert(error: .networkTaskError)
-                            print("networkTaskError")
-                        }
-                    }
-                }
-            }
+        //проверяем, есть ли кэш
+        //кэша нет
+        if cache.value(forKey: urlString) == nil {
+            fetchData()
         } else {
-            self.employeesSorted = cache.value(forKey: "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c")!
+            self.employeesSorted = cache.value(forKey: urlString) ?? []
             DispatchQueue.main.async {
                 self.employeesTableView.reloadData()
             }
         }
     }
+    
     
     //MARK: setting constraints for table view
     private func showActivityIndicator() {
@@ -114,6 +78,55 @@ final class EmployeerViewController: UIViewController {
         employeesTableView.backgroundColor = .white
     }
     
+    private func fetchData() {
+        employeesNetworkService.fetchData { [weak self] res in
+            self?.hideActivityIndicator()
+            guard let self = self else { return }
+            self.handle(result: res)
+        }
+    }
+    
+    private func handle(result: Result<CommonData, NetworkError>) {
+        switch result {
+        case .success(let value):
+            for employee in value.company.employees {
+                let employee = DataEmployeesModel(nameCompany: value.company.name,
+                                                  nameEmployees: employee.name,
+                                                  phoneEmployees: employee.phoneNumber,
+                                                  skillsEmployees: employee.skills)
+                
+                self.employees.append(employee)
+            }
+            
+            self.employeesSorted = (self.employees.sorted {
+                guard let firstName = $0.nameEmployees else { return false }
+                guard let secondName = $1.nameEmployees else { return true }
+                return firstName < secondName
+            })
+            
+            self.cache.insert(self.employeesSorted, forKey: urlString)
+            
+            DispatchQueue.main.async {
+                self.employeesTableView.reloadData()
+            }
+            
+        case .failure(let error):
+            switch error {
+            case .decodeError:
+                DispatchQueue.main.async {
+                    self.showAlert(error: .decodeError)
+                }
+            case .invalidUrl:
+                DispatchQueue.main.async {
+                    self.showAlert(error: .invalidUrl)
+                }
+            case .networkTaskError:
+                DispatchQueue.main.async {
+                    self.showAlert(error: .networkTaskError)
+                }
+            }
+        }
+    }
 }
 
 //MARK: add extension for uitableview
@@ -123,11 +136,11 @@ extension EmployeerViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "employeeCell", for: indexPath) as! EmployeesTableViewCell
-        cell.employeesCell = employeesSorted[indexPath.row]
-        cell.backgroundColor = .white
+        let cell = tableView.dequeueReusableCell(withIdentifier: "employeeCell", for: indexPath) as? EmployeesTableViewCell
+        cell?.employeesCell = employeesSorted[indexPath.row]
+        cell?.backgroundColor = .white
         
-        return cell
+        return cell ?? UITableViewCell()
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
